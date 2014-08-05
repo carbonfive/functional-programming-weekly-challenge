bd17aba994d0caed34b8eb733a170b745ec53111
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-08-04 23:59:59 -0700

    week 3 javascript: currying, partial function application, etc.

--- Week003/solution.js
+++ Week003/solution.js
@@ -1,66 +1,141 @@
 var $ = {};
 
-function compose(f, g) {
-  return function() {
-    return f.call(null, g.apply(null, arguments));
-  };
-};
+function curry(fx) {
+  var arity = fx.length;
 
-function concat(arr, value) {
-  return arr.concat(value);
+  return function() {
+    var args1 = Array.prototype.slice.call(arguments, 0),
+        len1  = args1.length;
+
+    if (len1 < arity) {
+      return function accumulator() {
+        var args2 = Array.prototype.slice.call(arguments, 0),
+            len2  = args2.length;
+
+        if (len1 + len2 == arity) {
+          return fx.apply(null, $.concat(args1, args2));
+        }
+        else {
+          return accumulator.apply(null, $.concat($.concat($.concat([], fx), args1), args2))
+        }
+      }
+    }
+    else {
+      return fx.apply(null, args1);
+    }
+  };
 }
 
-// foldr :: (a -> b -> b) -> b -> [a] -> b
-$.foldFromRight = function(f, acc, arr) {
-  if (arr.length == 0) {
+$.gt = curry(function(x, y) {
+  return x > y;
+});
+
+$.lt = curry(function(x, y) {
+  return x < y;
+});
+
+$.div = curry(function(n, d) {
+  return (n / d);
+});
+
+$.sum = curry(function(x, y) {
+  return (x + y);
+});
+
+$.mul = curry(function(x, y) {
+  return (x * y);
+});
+
+$.mod = curry(function(x, y) {
+  return x % y;
+});
+
+$.id = function(x) {
+  return x;
+};
+
+$.eq = curry(function(x, y) {
+  return x === y;
+});
+
+$.concat = curry(function(arr, value) {
+  return arr.concat(value);
+});
+
+$.comp = curry(function(f, g) {
+  return function() {
+    return f.call(null, g.apply(null, Array.prototype.slice.call(arguments, 0)));
+  };
+});
+
+$.arg0 = function() {
+  return $.id.apply(null, Array.prototype.slice.call(arguments, 0, 1));
+};
+
+$.arg1 = function() {
+  return $.id.apply(null, Array.prototype.slice.call(arguments, 1, 2));
+};
+
+$.flip = function(f) {
+  return curry(function(x, y) {
+    return f.apply(null, [y, x]);
+  });
+};
+
+$.iif = curry(function(expr, f1, f2) {
+  return function() {
+    var expr2 = (typeof expr == 'function') ? expr : function() { return expr; }
+
+    if (expr2.apply(null, arguments)) {
+      return f1.apply(null, arguments);
+    }
+    else {
+      return f2.apply(null, arguments);
+    }
+  };
+});
+
+// TODO: fix O(n^2)
+$.foldFromRight = curry(function(f, acc, arr) {
+  var lastIdx = arr.length - 1;
+
+  if (lastIdx === -1) {
     return acc;
   }
   else {
-    var lastIdx = arr.length - 1;
     return $.foldFromRight(f, f(arr[lastIdx], acc), arr.slice(0, lastIdx));
   }
-};
+});
 
-$.foldFromLeft = function(f, acc, arr) {
-  if (arr.length == 0) {
+$.foldFromLeft = curry(function(f, acc, arr) {
+  if (arr[0] == undefined) {
     return acc;
   }
   else {
     return $.foldFromLeft(f, f(acc, arr[0]), arr.slice(1, arr.length));
   }
-};
+});
 
-$.mapFromLeft = function(f, arr) {
+$.mapFromLeft = curry(function(f, arr) {
   return $.foldFromLeft(function(acc, item) {
-    return acc.concat(f(item));
+    return $.concat(acc, f(item));
   }, [], arr);
-};
+});
 
-$.filterFromLeft = function(p, arr) {
-  return $.foldFromLeft(function(acc, item) {
-    if (p(item)) {
-      return acc.concat(item);
-    }
-    else {
-      return acc;
-    }
-  }, [], arr);
-};
+$.filterFromLeft = curry(function(p, arr) {
+  return $.foldFromLeft($.iif($.comp(p, $.arg1), $.concat, $.id), [], arr);
+});
 
-$.findFromLeft = function(p, arr) {
+$.findFromLeft = curry(function(p, arr) {
   var len = arr.length;
 
   if (len == 0) {
     return undefined;
   }
-  else {
-  }
-  if (p(arr[0])) {
+  else if (p(arr[0])) {
     return arr[0];
   }
   else {
     return $.findFromLeft(p, arr.slice(1, len));
   }
-};
-
-module.exports = $
+});
--- Week003/test.js
+++ Week003/test.js
@@ -1,5 +1,3 @@
-$ = require('./solution.js');
-
 function assertEq(expr1, expr2, msg) {
   if (JSON.stringify(expr1) !== JSON.stringify(expr2)) {
     throw new Error("*** was not equal: " + msg + "\n" + "expr1: " + expr1 + " and expr2: " + expr2 + "\n\n");
@@ -9,49 +7,27 @@ function assertEq(expr1, expr2, msg) {
   }
 }
 
-// foldr
-// (a -> b -> b) -> b -> [a] -> b
-one = $.foldFromRight(function(item, acc) {
-  return acc + item;
-}, 0, [1, 2, 3, 4, 5]);
+function isUpperCase(s) {
+  return s.toUpperCase() == s;
+};
 
-// foldl
-// (a -> b -> a) -> a -> [b] -> a
-two = $.foldFromLeft(function(acc, item) {
-  if (item == item.toUpperCase()) {
-    return acc;
-  }
-  else {
-    return acc.concat(item);
-  }
-}, [], "Hello, Moto".split(""));
-
-// map
-// (a -> b) -> [a] -> [b]
-three = $.mapFromLeft(function(item) {
-  return item * 10;
-}, [1, 2, 3, 4]);
-
-fourA = $.filterFromLeft(function(item) {
-  return item % 2 == 1;
-}, [1, 2, 3, 4]);
-
-fourB = $.filterFromLeft(function(item) {
-  return item > 10;
-}, [1, 2, 3, 4]);
-
-fiveA = $.findFromLeft(function(item) {
-  return item == 2;
-}, [1, 2, 3, 4]);
-
-fiveB = $.findFromLeft(function(item) {
-  return item == 10;
-}, [1, 2, 3, 4]);
-
+var one = $.foldFromRight($.sum, 0, [1, 2, 3, 4, 5])
 assertEq(one, 15, "foldr sum");
+
+var two = $.foldFromLeft($.iif($.comp(isUpperCase, $.arg1), $.id, $.concat), [], "Hello, Moto".split(""));
 assertEq(two, "ellooto".split(""), "foldl only lowercase alpha chars");
+
+var three = $.mapFromLeft($.mul(10), [1, 2, 3, 4])
 assertEq(three, [10, 20, 30, 40], "map from left *10");
+
+var fourA = $.filterFromLeft($.comp($.eq(1), $.flip($.mod)(2)), [1, 2, 3, 4])
 assertEq(fourA, [1,3], "filter out even values");
+
+var fourB = $.filterFromLeft($.lt(10), [1, 2, 3, 4]);
 assertEq(fourB, [], "filters everything out");
+
+var fiveA = $.findFromLeft($.eq(2), [1, 2, 3, 4]);
 assertEq(fiveA, 2, "match w/find");
+
+var fiveB = $.findFromLeft($.eq(10), [1, 2, 3, 4]);
 assertEq(fiveB, undefined, "no match w/find");
